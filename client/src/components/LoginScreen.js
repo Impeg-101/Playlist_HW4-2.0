70bce655cc20bb1ff18044bc3f8684b188643619
EheTeNanDaYoPaimon <98989799+EheTeNanDaYoPaimon@users.noreply.github.com>
2022-11-07 15:53:52 -0500

    included error modal

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -15,6 +15,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import AccountError from './AccountError';
 
 export default function LoginScreen() {
     const { auth } = useContext(AuthContext);
@@ -30,6 +31,7 @@ export default function LoginScreen() {
     };
 
     return (
+        <div>
         <Grid container component="main" sx={{ height: '100vh' }}>
             <CssBaseline />
             <Grid
@@ -112,5 +114,6 @@ export default function LoginScreen() {
                 </Box>
             </Grid>
         </Grid>
+        <AccountError/></div>
     );
 }
